480aa32abbaf260c9304e5022d04294ca8e772eb
Choi Hwi bin <chlgnlqls3@nate.com>
2024-01-23 22:16:01 +0900

    feat: Jpa를 사용해 MySQL 연동

--- pass-batch/src/main/java/com/hwibin/prac/passbatch/repository/packaze/PackageEntity.java
+++ pass-batch/src/main/java/com/hwibin/prac/passbatch/repository/packaze/PackageEntity.java
@@ -1,11 +1,12 @@
 package com.hwibin.prac.passbatch.repository.packaze; // package는 reserved word로 사용할 수 없어서 packaze를 사용합니다.
 
-import com.fastcampus.pass.repository.BaseEntity;
+
+import com.hwibin.prac.passbatch.repository.BaseEntity;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-import javax.persistence.*;
 
 @Getter
 @Setter
